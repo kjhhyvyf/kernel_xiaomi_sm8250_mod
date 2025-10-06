1ebd8d9b57b8a9a64c842758cf0380c1c824db54
烤坤 <3332539702@qq.com>
2025-10-07 00:31:04 +0800

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -2,11 +2,13 @@
 set -e
 
 bash build.sh psyche
+bash build.sh thyme
 bash build.sh umi
 bash build.sh munch
 bash build.sh lmi
 bash build.sh cmi
 bash build.sh cas
+bash build.sh apollo
 bash build.sh alioth
 bash build.sh elish
 bash build.sh enuma
@@ -14,13 +16,29 @@ bash build.sh dagu
 bash build.sh pipa
 
 bash build.sh psyche ksu
+bash build.sh thyme ksu
 bash build.sh umi ksu
 bash build.sh munch ksu
 bash build.sh lmi ksu
 bash build.sh cmi ksu
 bash build.sh cas ksu
+bash build.sh apollo ksu
 bash build.sh alioth ksu
 bash build.sh elish ksu
 bash build.sh enuma ksu
 bash build.sh dagu ksu
 bash build.sh pipa ksu
+
+bash build.sh psyche ksu kpm
+bash build.sh thyme ksu kpm
+bash build.sh umi ksu kpm
+bash build.sh munch ksu kpm
+bash build.sh lmi ksu kpm
+bash build.sh cmi ksu kpm
+bash build.sh cas ksu kpm
+bash build.sh apollo ksu kpm
+bash build.sh alioth ksu kpm
+bash build.sh elish ksu kpm
+bash build.sh enuma ksu kpm
+bash build.sh dagu ksu kpm
+bash build.sh pipa ksu kpm
